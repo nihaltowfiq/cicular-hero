b5ee1b9c17040a8405b78d2c1749a2d9287bbac4
nihaltowfiq <nihaltowfiq@gmail.com>
2024-10-22 23:58:58 +0600

    deleted

--- src/utils.ts
+++
@@ -1,8 +0,0 @@
-import { type ClassValue, clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
-
-function classNames(...values: ClassValue[]) {
-  return twMerge(clsx(values));
-}
-
-export default classNames;
